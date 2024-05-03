72e0536f8e11c5f4485096687a1cc2b0d4a0a8ad
JuegoBuscaminas <mafevarey@hotmail.com>
2024-05-02 19:53:05 -0500

    Clase prestamo creada por yakelin ariza

---
+++ JavaApplication21/src/javaapplication21/Prestamo_Jackie.java
@@ -0,0 +1,19 @@
+
+package javaapplication21;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Prestamo_Jackie {
+
+  
+    public ArrayList libro = new ArrayList<Libro>();
+    public Cliente ocliente;
+    public Date fecPrestamo;
+    public Date fecDevolucion;
+    
+  
+    public int CalcularMulta(){
+        return 0;
+    }
+}

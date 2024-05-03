9bac462082f0c60d02bbf292a57c3c483e9f5402
JuegoBuscaminas <mafevarey@hotmail.com>
2024-05-02 19:59:02 -0500

    Clase creada por erika

---
+++ JavaApplication21/src/javaapplication21/Prestamo.java
@@ -0,0 +1,20 @@
+package javaapplication21;
+
+import Biblioteca.Cliente;
+import Biblioteca.Libro;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Prestamo {
+
+    public ArrayList Libro = new ArrayList<Libro>();
+    public Cliente oCliente;
+    public Date fecPrestamo;
+    public Date fecDevolucion;
+            
+    public int calcularMulta() {
+        return 0;
+
+    }
+
+}
